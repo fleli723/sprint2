a88305a41f00b47fc8d26c8fe4d42a02445b0f52
Lelis <fleli723@uwsp.edu>
2019-11-25 00:00:50 -0600

    fixed search WS

--- searchProcessing.php
+++ searchProcessing.php
@@ -1,11 +1,7 @@
 <?php
-
 session_start();
 require_once("classes/DB.class.php");
-
 $userData = file_get_contents("php://input");
-
-
 if (is_null($userData) || empty($userData)) 
 {
 	print "You must enter valid values";
@@ -23,20 +19,15 @@ else
 	}
 	else
 	{
-
 		//Sanitize the user input
 		$search = json_decode($userData);
-
 		
-		//$result = $con->dbCall($query);
-		//Check for DB Insert Errors
+		$searchTerm = $con->dbESC($search);
 		//query the db for the search results	
 		$query = "SELECT * FROM albums WHERE albums.albumArtist LIKE '%$searchTerm%' or albums.AlbumTitle LIKE '%$searchTerm%'";
 		
 		$result = $con->dbCall($query);
 		print json_encode($result);
 	}
-
 }
-
 ?>
